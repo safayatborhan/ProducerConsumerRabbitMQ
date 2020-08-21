7afa0c237a5ec9e301ad72aa24a710ea7e360342
Safayat Zisan <zisan_cse_mbstu@hotmail.com>
2020-08-21 20:41:33 +0600

    initial commit

---
+++ ProducerConsumerRabbitMQ/ProducerConsumerRabbitMQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProducerConsumerRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
